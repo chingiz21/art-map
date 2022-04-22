5e72ab55600d68c9580d419df0d1d0f23cd8b25d
Chingiz Zaripov <chingiz.zaripov2000@gmail.com>
2022-04-22 14:23:00 +0200

    added auth
    created user authorization, added roles and token middlewares, added endpoints on userController

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ const sequelize = require('./db');
 const fileUpload = require('express-fileupload');
 const models = require('./models/models');
 const router = require('./routes/index');
+const errorHandler = require('./middleware/ErrorHandlingMiddleware');
 
 
 const PORT = process.env.PORT || 5000;
@@ -15,6 +16,7 @@ app.use(cors());
 app.use(express.json());
 app.use(fileUpload({}));
 
+app.use(errorHandler);
 
 
 app.use('/api', router);
--- backend/routes/user-route.js
+++ backend/routes/user-route.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
 const UserController = require('../controllers/UserContoller');
+const authMiddleware = require('../middleware/authMiddleware');
 
 router.post('/registration', UserController.registration);
 router.post('/login', UserController.login);
-router.get('/auth', UserController.checkAuth);
+router.get('/check', authMiddleware, UserController.check);
 
 module.exports = router;
